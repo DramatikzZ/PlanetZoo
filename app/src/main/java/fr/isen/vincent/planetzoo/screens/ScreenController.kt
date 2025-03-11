ab7b00f4c258ccada2ec1ee653823b0b4bdb2891
DramatikzZ <clevin13@gmail.com>
2025-03-11 14:02:54 +0100

    clean

--- app/src/main/java/fr/isen/vincent/planetzoo/screens/ScreenController.kt
+++ app/src/main/java/fr/isen/vincent/planetzoo/screens/ScreenController.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
@@ -22,7 +21,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import fr.isen.vincent.planetzoo.R
